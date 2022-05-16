74dbe76e5c2f45ec877b9dfdb6a7d35310344ea6
Tyangs <t.youngs0324@gmail.com>
2022-05-16 22:50:47 +0800

    :art: (eslint-format) add eslint config & format code

--- src/utils/flatten.ts
+++ src/utils/flatten.ts
@@ -1,5 +1,6 @@
-import { isArray, isObject } from './assertion';
 import { Dict } from '@/types';
+
+import { isArray, isObject } from './assertion';
 
 export const flatten = (values: Dict, maxDepth = Infinity) => {
   // 当 values 为非对象或数组，或者已递归了 maxDepth 次，则退出递归
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export * from './object';
 export * from './assertion';
 export * from './flatten';
+export * from './object';
