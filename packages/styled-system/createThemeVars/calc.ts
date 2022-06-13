e8993d23e2acd34f8dd6ca0ad13c089357dbb16b
Tyangs <t.youngs0324@gmail.com>
2022-06-13 23:31:54 +0800

    :speech_balloon: (calc-note) calc file add some note

--- packages/styled-system/createThemeVars/calc.ts
+++ packages/styled-system/createThemeVars/calc.ts
@@ -1,3 +1,7 @@
+/*
+ * Provides util functions for generating css calc.
+ */
+
 import { isObject } from '@youngs-ui/utils';
 
 export type Reference = { reference: string };
@@ -6,29 +10,65 @@ export type Operand = string | number | Reference;
 
 export type Operator = '+' | '-' | '*' | '/';
 
-const resolveReference = (operand: Operand): string => {
+/**
+ * resolve operand to string
+ * @param operand
+ * @returns the resolved operand string
+ */
+const resolveOperand = (operand: Operand): string => {
   if (isObject(operand) && operand.reference) {
     return operand.reference;
   }
   return String(operand);
 };
 
+/**
+ * combining `operator` and `operands` to be expression string
+ * @param operator css calc operator
+ * @param operands operand array
+ * @returns expression string that will expressed by calc
+ */
 const getExpression = (operator: Operator, ...operands: Array<Operand>) =>
   operands
-    .map(resolveReference)
+    .map(resolveOperand)
     .join(` ${operator} `)
     .replace(/calc/g, '');
 
+/**
+ * get css calc add expression
+ * @param operands operand array
+ * @returns css calc add expression
+ */
 const add = (...operands: Array<Operand>) => `calc(${getExpression('+', ...operands)})`;
 
+/**
+ * get css calc subtract expression
+ * @param operands operand array
+ * @returns css calc subtract expression
+ */
 const subtract = (...operands: Array<Operand>) => `calc(${getExpression('-', ...operands)})`;
 
+/**
+ * get css calc multiply expression
+ * @param operands operand array
+ * @returns css calc multiply expression
+ */
 const multiply = (...operands: Array<Operand>) => `calc(${getExpression('*', ...operands)})`;
 
+/**
+ * get css calc divide expression
+ * @param operands operand array
+ * @returns css calc divide expression
+ */
 const divide = (...operands: Array<Operand>) => `calc(${getExpression('/', ...operands)})`;
 
+/**
+ * get negate value
+ * @param operand operand
+ * @returns operand negate value
+ */
 const negate = (operand: Operand) => {
-  const value = resolveReference(operand);
+  const value = resolveOperand(operand);
 
   if (!Number.isNaN(value)) {
     return value.startsWith('-') ? value.slice(1) : `-${value}`;
@@ -46,6 +86,9 @@ interface CalcChain {
   toString: () => string;
 }
 
+/**
+ * an object that provide calc function and calc chain function
+ */
 export const calc = Object.assign(
   (x: Operand): CalcChain => ({
     add: (...operands) => calc(add(x, ...operands)),
