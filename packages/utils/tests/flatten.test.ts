8a41da784dec4334220d9bc168ff96988974a694
Tyangs <t.youngs0324@gmail.com>
2022-06-14 23:23:28 +0800

    :sparkles: (update-flatten) update flatten function

--- packages/utils/tests/flatten.test.ts
+++ packages/utils/tests/flatten.test.ts
@@ -3,26 +3,32 @@ import { flatten } from '../src/flatten';
 describe('flatten', () => {
   it('should flatten an object correctly', () => {
     const object = { colors: { red: { 500: '#ff0000' } } };
-    const expectResult = { 'colors.red': { '500': '#ff0000' } };
 
     const flatResult = flatten(object, 1);
-    expect(flatResult).toEqual(expectResult);
+    expect(flatResult).toMatchInlineSnapshot(`
+      Object {
+        "colors.red": Object {
+          "500": "#ff0000",
+        },
+      }
+    `);
   });
   it('should flatten an array correctly', () => {
     const array = {
       space: [0, 1, 2, 4, 8, 16, 32],
     };
-    const expectResult = {
-      'space.0': 0,
-      'space.1': 1,
-      'space.2': 2,
-      'space.3': 4,
-      'space.4': 8,
-      'space.5': 16,
-      'space.6': 32,
-    };
 
     const flatResult = flatten(array, 1);
-    expect(flatResult).toEqual(expectResult);
+    expect(flatResult).toMatchInlineSnapshot(`
+      Object {
+        "space.0": 0,
+        "space.1": 1,
+        "space.2": 2,
+        "space.3": 4,
+        "space.4": 8,
+        "space.5": 16,
+        "space.6": 32,
+      }
+    `);
   });
 });
